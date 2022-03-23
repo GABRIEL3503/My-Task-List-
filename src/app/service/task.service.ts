bdc25cbb2620bcb62ac204b554e6d793dcacfbce
gabriel <you@example.com>
2022-03-23 20:46:42 -0300

    json

---
+++ src/app/service/task.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Task } from '../components/Task';
+import {TASKS} from '../components/mock-tasks'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor() { }
+  getTasks(): Observable< Task[] >{
+    const tasks = of (TASKS);
+    return tasks
+  }
+}
